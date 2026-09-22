f28a3785af33632df6d9c68abec0b7322b03f5d3
jingwangjw81 <jingwangjw81@gmail.com>
2026-09-22 12:56:50 +0000

    Change back to working model codestral-2508

--- src/smoke_test.py
+++ src/smoke_test.py
@@ -16,9 +16,8 @@ if ok_g:
 
 if ok_m:
     from llama_index.llms.mistralai import MistralAI
-    r = MistralAI(model="mistral-small-2506").complete("Antworte mit einem Wort: Bern?") # codestral-2508
-    # print("Mistral:", str(r).strip())
-    print(r)
+    r = MistralAI(model="codestral-2508").complete("Antworte mit einem Wort: Bern?") # Not working: mistral-small-2506, mistral-small-latest
+    print("Mistral:", str(r).strip())
 
     # llm = MistralAI(model="mistral-small-latest")
     # response = llm.complete("Hello")
